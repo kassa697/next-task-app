13ffce1745b3b8b70b7211a0011f118df39e599a
0058650_笠原　恭平 <kyohei_kasahara@dk.daihatsu.co.jp>
2024-10-29 12:59:03 +0900

    edit delete button add

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,20 +1,34 @@
 import React from "react";
+import TaskEditButton from "./TaskEditButton/TaskEditButton";
+import TaskDeleteButton from "./TaskDeleteButton/TaskDeleteButton";
 
 const TaskCard = () => {
   return (
-    <div>
+    <div
+      className="w-64 h-52 p-4 bg-white rounded-md shadow-md 
+    flex flex-col justify-between"
+    >
       <header>
-        <h1>Title</h1>
-        <div>Task Description</div>
+        <h1 className="text-lg font-semibold">Title</h1>
+        <div className="mt-1 text-sm line-clamp-3">Task Description</div>
       </header>
       <div>
-        <div>
+        <div className="text-sm">
           2024-11-30
-          <div>
-            <div>{true ? "Completed" : "Incomplete"}</div>
+          <div className="flex justify-between items-center">
+            <div
+              className={`mt-1 text-sm px-2 py-1 w-24 text-center text-white
+            rounded-full shadow-sm ${true ? "bg-green-500" : "bg-red-500"}`}
+            >
+              {true ? "Completed" : "Incomplete"}
+            </div>
             <div>
-              <div>Edit</div>
-              <div>Delete</div>
+              <div>
+                <TaskEditButton id="1" />
+              </div>
+              <div>
+                <TaskDeleteButton id="1" />
+              </div>
             </div>
           </div>
         </div>
